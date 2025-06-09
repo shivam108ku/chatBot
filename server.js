4bf6710fa43ffb4a543ad3ed88e7788ee59b12da
shivam108ku <shivam108ku@gmail.com>
2025-06-09 15:10:38 +0530

    last

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ import chatRoute from './routes/chatRoute.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173'  // or your frontend URL
+}));
 app.use(express.json());
 app.use('/api', chatRoute);
 
